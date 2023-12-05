b17618eda707b68c52897065b8fd18ad3d7c3e7c
EleazarRamirez <101959194+EleazarRamirez@users.noreply.github.com>
2023-12-04 18:50:07 -0800

    Add files via upload
    agrego de nuevo estos archivos ya que han sido modificados un poco, el de palabrasR eh agregado nuevas palabras. en Texto eh cambiado la sintaxis(solo por pruebas). y en el sintactico eh agregado mas tokens en el arreglo de arreglos para validar la sintaxis

--- sintactico.js
+++ sintactico.js
@@ -8,29 +8,43 @@ function revisarExpresion(expresion,palabras) {
     let index = 0;
 
     const arreglosDeID = {
-          21: [23,41,43,44,7,45,47],
-          22: [23,2],
-          23: [22,8,30,4,98,99,9,10,37,2,3,57,21,8,26,32,33,34,31,36,1,5,6,48,40],
-          7: [22,23,24],
-          5: [23],
-          24:[27,1],
-          26: [23,187],
-          8: [19,23,24,2],
-          19: [25,19,4,1,3],
-          25: [19],
-          4: [3,1,23,43],
-          3:[30,9,10,8],
-          41:[23,186],
-          37:[38],
-          38:[23,187,188],
-          42:[24],
-          39:[38],
-          40:[23,43],
-          31:[23,2],
-          32:[31],
-          33:[31],
-          34:[31],
-          1: [1]
+          21: [23,41,43,44,7,45,47],    //SELECT
+          22: [23,2],   //FROM
+          23: [22,8,30,4,98,99,9,10,37,2,3,57,21,8,26,32,33,34,31,36,1,5,6,48,40,106,107],  //PALABRA
+          7: [22,23,24],    //*
+          5: [23],  // . PUNTO
+          24:[27,1],    //NUMERO
+          26: [23,187], //WHERE
+          8: [19,23,24,2],     //= IGUAL
+          19: [25,19,4,1,3],    // ' COMILLA SIMPLE
+          25: [19], //TEXTO
+          4: [3,1,23,43],   // , COMA
+          3:[30,9,10,8],    // )         
+          41:[23,186],  //DISTINCT
+          37:[38],  //ORDER
+          38:[23,187,188],  //BY
+          42:[24],  //LIMIT
+          39:[38],  //GROUP
+          40:[23,43],   //HAVING
+          31:[23,2],    //JOIN
+          32:[31],  //INNER
+          33:[31],  //LEFT
+          34:[31],  //RIGHT
+          43:[2],   //COUNT
+          44:[2],   //AVG
+          45:[2],   //SUM
+          46:[2],   //MAX
+          47:[2],   //MIN
+          30:[23],  //AS
+          51:[19],  //LIKE
+          52:[2,19],    //IN
+          50:[24],  //BETWEEN
+          48:[49,29],   //IS
+          29:[49],  //NOT
+          49:[1,27],    //NULL
+          106:[1],  //DESC
+          107:[1],  //ASC
+          1: [1]    // ; punto y coma
         };
 
     //funcion para validar los componentes validos
@@ -57,6 +71,9 @@ function revisarExpresion(expresion,palabras) {
     }
 
 function compararPalabra(comp){
+    // Esta función recorre el array 'palabrasReservadas' y compara cada elemento con 'comp'.
+    // Si encuentra una coincidencia, agrega el índice + 1 al array 'clavesEncontradas' y devuelve true.
+    // Si no encuentra coincidencias, agrega 23 al array 'clavesEncontradas' y devuelve false.
     for(let i=0; i<palabrasReservadas.length;i++){
         if(palabrasReservadas[i] == comp){
             clavesEncontradas.push(i+1);
@@ -68,21 +85,19 @@ function compararPalabra(comp){
 }
 
 function validarSintaxis(){
+    // Esta función recorre el array 'clavesEncontradas' y extrae cada token en el arreglo 'arreglosDeID' en la posición 'clavesEncontradas[j]'.
+    // Luego, recorre cada token buscando el siguiente elemento en 'clavesEncontradas'. Si encuentra una coincidencia, significa que la sintaxis es correcta.
+    // Si no encuentra coincidencias, indica que hay un error de sintaxis.
     for(let j=0; j<clavesEncontradas.length; j++){
             const token = arreglosDeID[clavesEncontradas[j]];
            if(token != null){
             let k=0;
-            let bandera = 0;
                 while(token[k] != clavesEncontradas[j+1]){
                     if(k < token.length){
                         k++;
                     }else{
-                        bandera=1;
                         console.log("Error de Sintaxis");
                         break;
-                    }
-                    if(j === clavesEncontradas.length-1 && bandera === 0){
-                        console.log("Sintaxis Correcto!");
                     }
                 }
             }
@@ -130,7 +145,7 @@ function validarSintaxis(){
         }
     }
     analizar();
-    //console.log(clavesEncontradas);
+    console.log(clavesEncontradas);
     validarSintaxis();
 
         
